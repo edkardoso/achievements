0e0f73c4a0fc299c120f5266ac7463504efeeb71
leonardo <leonardo@Developer.(none)>
2011-11-25 22:56:28 -0200

    Add project

---
+++ src/Achievements.Tests/AchievementsTests.cs
@@ -0,0 +1,28 @@
+using System;
+using NUnit.Framework;
+
+namespace Achievements.Tests
+{
+	[TestFixture]
+	public class AchievementsTests
+	{
+		
+
+		
+		[Test]
+		public void AAA()
+		{
+			Assert.AreEqual(1,1);
+		}
+		
+				
+		[Test]
+		public void BBA()
+		{
+
+		}
+		
+		
+	}
+}
+
