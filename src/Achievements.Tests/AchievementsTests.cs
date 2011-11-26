db08ceae7da1f64263255f7de4d047410a66ac59
Leonardo Costa <leotasco@gmail.com>
2011-11-26 02:01:13 -0200

    Updates

--- src/Achievements.Tests/AchievementsTests.cs
+++ src/Achievements.Tests/AchievementsTests.cs
@@ -1,26 +1,114 @@
 
+using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace Achievements.Tests
 {
 	[TestFixture]
-	public class AchievementsTests
+	public class AchievementTests
 	{
 				
 		[Test]
-		public void AAA()
+		public void Testando()
 		{
 			Assert.AreEqual(1,1);
 		}
 		
-				
-		[Test]
-		public void BBA()
-		{
+		/*
+		 * 1 - Quando abrir popup, buscar missao pendente do jogador (IsCompleted == false)
+		 * 2 - Se encontrar, verificar se ele concluiu 
+		 * 3 - Caso tenha concluido, incluir nova missao
+		 * 4 - Exibir missao pendente
+		 * 
+		 * A partir da 10a. missao, o jogador poderá escolher a missao que desejar
+		 */
 
-		}
 		
 		
 	}
+
+	/// <summary>
+	/// State pattern
+	/// </summary>
+	public class Achievement
+	{
+		private MissionState _missionState;
+		
+		public void Show()
+		{
+			_missionState.VerificarMissaoConcluida(this);
+		}
+
+		public MissionState CurrentMission { get; set; }
+	}
+
+
+	public abstract class MissionState
+	{
+		public abstract bool VerificarMissaoConcluida(Achievement achievement);
+	}
+
+
+	public class Missao1 : MissionState
+	{
+		public override bool VerificarMissaoConcluida(Achievement achievement)
+		{
+			if (true) //Se concluida
+			{
+				achievement.CurrentMission = new Missao2();
+			}
+			return true;
+		}
+	}
+
+	public class Missao2 : MissionState
+	{
+		public override bool VerificarMissaoConcluida(Achievement achievement)
+		{
+			return true;
+		}
+	}
+
+
+
+
+
+	//POCOs
+
+	class XAchievement
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+	}
+
+
+	class XPlayer
+	{
+		public int Id { get; set; }
+		public ICollection<PlayerAchievement> Achievements { get; set; }
+	}
+
+	class PlayerAchievement
+	{
+
+		public PlayerAchievement()
+		{
+
+		}
+
+		public PlayerAchievement(XPlayer player)
+		{
+
+		}
+
+		public int Id { get; set; }
+		public XPlayer Player { get; set; }
+		public XAchievement Achievement { get; set; }
+		public bool IsCompleted { get; set; }
+	}
+
+	
 }
 
--- src/Achievements/Main.cs
+++ src/Achievements/Main.cs
@@ -5,181 +5,31 @@ namespace Achievements
 {
 	class MainClass
 	{
-		private static List<Achievement> _achievementsList;
-		
+
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Starting Achievements...");
 			
-			_achievementsList = new List<Achievement>() 
-			{
-				new Achievement(){ Id = 1, Name = "Contratar trabalhadores até um mínimo de 15", Description = "Você precisa de trabalhadores para produzir recursos blablabla"},
-				new Achievement(){ Id = 2, Name = "Iniciar pesquisa de qualquer classe nivel 1", Description = "Você precisa de trabalhadores para produzir recursos blablabla"},
-				new Achievement(){ Id = 3, Name = "Contratar trabalhadores até um mínimo de 15", Description = "Você precisa de trabalhadores para produzir recursos blablabla"},
-				new Achievement(){ Id = 4, Name = "Contratar trabalhadores até um mínimo de 15", Description = "Você precisa de trabalhadores para produzir recursos blablabla"},
-				new Achievement(){ Id = 5, Name = "Contratar trabalhadores até um mínimo de 15", Description = "Você precisa de trabalhadores para produzir recursos blablabla"}
-			};
+			//_achievementsList = new List<Achievement>() 
+			//{
+			//    new Achievement(){ Id = 1, Name = "Contratar trabalhadores até um mínimo de 15", Description = "Você precisa de trabalhadores para produzir recursos blablabla"},
+			//    new Achievement(){ Id = 2, Name = "Iniciar pesquisa de qualquer classe nivel 1", Description = "Você precisa de trabalhadores para produzir recursos blablabla"},
+			//    new Achievement(){ Id = 3, Name = "Contratar trabalhadores até um mínimo de 15", Description = "Você precisa de trabalhadores para produzir recursos blablabla"},
+			//    new Achievement(){ Id = 4, Name = "Contratar trabalhadores até um mínimo de 15", Description = "Você precisa de trabalhadores para produzir recursos blablabla"},
+			//    new Achievement(){ Id = 5, Name = "Contratar trabalhadores até um mínimo de 15", Description = "Você precisa de trabalhadores para produzir recursos blablabla"}
+			//};
 			
-			var player = new Player { Id = 1 };
+			//var player = new Player { Id = 1 };
 			
-			var playerAchievement =  new PlayerAchievement(player);
+			//var playerAchievement =  new PlayerAchievement(player);
 			
 			
-			
-		}
-		
-		
-		
-	}
-	
-	
-	
-	
-	public class AudioPlayer
-	{
-	    private AudioPlayerState _state;
-	
-	    public AudioPlayer(AudioPlayerState state)
-	    {
-	        _state = state;
-	    }
-	
-	    public void PressPlay()
-	    {
-	        _state.PressPlay(this);
-	    }
-	
-	    public void PressAudioSource()
-	    {
-	        _state.PressAudioSource(this);
-	    }
-	
-	    public AudioPlayerState CurrentState
-	    {
-	        get { return _state; }
-	        set { _state = value; }
-	    }
-	}
-	
-	
-	public abstract class AudioPlayerState
-	{
-	    public abstract void PressPlay(AudioPlayer player);
-	
-	    public abstract void PressAudioSource(AudioPlayer player);
-	}
-	
-	
-	public class StandbyState : AudioPlayerState
-	{
-	    public StandbyState()
-	    {
-	        Console.WriteLine("STANDBY");
-	    }
-	
-	    public override void PressPlay(AudioPlayer player)
-	    {
-	        Console.WriteLine("Play pressed: No effect");
-	    }
-	
-	    public override void PressAudioSource(AudioPlayer player)
-	    {
-	        player.CurrentState = new MP3PlayingState();
-	    }
-	}
-	
-	
-	public class MP3PlayingState : AudioPlayerState
-	{
-	    public MP3PlayingState()
-	    {
-	        Console.WriteLine("MP3 PLAYING");
-	    }
-	
-	    public override void PressPlay(AudioPlayer player)
-	    {
-	        player.CurrentState = new MP3PausedState();
-	    }
-	
-	    public override void PressAudioSource(AudioPlayer player)
-	    {
-	        player.CurrentState = new RadioState();
-	    }
-	}
-	
-	
-	public class MP3PausedState : AudioPlayerState
-	{
-	    public MP3PausedState()
-	    {
-	        Console.WriteLine("MP3 PAUSED");
-	    }
-	
-	    public override void PressPlay(AudioPlayer player)
-	    {
-	        player.CurrentState = new MP3PlayingState();
-	    }
-	
-	    public override void PressAudioSource(AudioPlayer player)
-	    {
-	        player.CurrentState = new RadioState();
-	    }
-	}
-	
-	
-	public class RadioState : AudioPlayerState
-	{
-	    public RadioState()
-	    {
-	        Console.WriteLine("RADIO");
-	    }
-	
-	    public override void PressPlay(AudioPlayer player)
-	    {
-	        Console.WriteLine("Play pressed: New station selected");
-	    }
-	
-	    public override void PressAudioSource(AudioPlayer player)
-	    {
-	        player.CurrentState = new StandbyState();
-	    }
-	}
-		
-		
-	
-	//POCO's
-	
-	class Achievement 
-	{
-		public int Id {get;set;}
-		public string Name {get;set;}
-		public string Description {get;set;}
-	}
-
-	
-	class Player
-	{
-		public int Id {get;set;}
-		public ICollection<PlayerAchievement> Achievements {get;set;}
-	}
-	
-	class PlayerAchievement
-	{
-		
-		public PlayerAchievement(){
 			
 		}
 		
-		public PlayerAchievement(Player player){
 		
-		}
-	
-		public int Id {get;set;}
-		public Player Player {get;set;}
-		public Achievement Achievement {get;set;}
-		public bool IsCompleted {get;set;} 
+		
 	}
-
 	
 
 
