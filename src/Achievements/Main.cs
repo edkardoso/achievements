403ba2e975ee59695a569f0f97dede42cbca1ae6
Leonardo Costa <leotasco@gmail.com>
2011-11-28 18:16:43 -0200

    Refactoring

--- src/Achievements/Main.cs
+++ src/Achievements/Main.cs
@@ -9,29 +9,8 @@ namespace Achievements
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Starting Achievements...");
-			
-			//_achievementsList = new List<Achievement>() 
-			//{
-			//    new Achievement(){ Id = 1, Name = "Contratar trabalhadores até um mínimo de 15", Description = "Você precisa de trabalhadores para produzir recursos blablabla"},
-			//    new Achievement(){ Id = 2, Name = "Iniciar pesquisa de qualquer classe nivel 1", Description = "Você precisa de trabalhadores para produzir recursos blablabla"},
-			//    new Achievement(){ Id = 3, Name = "Contratar trabalhadores até um mínimo de 15", Description = "Você precisa de trabalhadores para produzir recursos blablabla"},
-			//    new Achievement(){ Id = 4, Name = "Contratar trabalhadores até um mínimo de 15", Description = "Você precisa de trabalhadores para produzir recursos blablabla"},
-			//    new Achievement(){ Id = 5, Name = "Contratar trabalhadores até um mínimo de 15", Description = "Você precisa de trabalhadores para produzir recursos blablabla"}
-			//};
-			
-			//var player = new Player { Id = 1 };
-			
-			//var playerAchievement =  new PlayerAchievement(player);
-			
-			
-			
 		}
-		
-		
-		
 	}
-	
-
 
 }
 
